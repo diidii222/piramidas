9a8f6c93e57417e6ec7d778152a0043621b24afd
diidii222 <123403223+diidii222@users.noreply.github.com>
2023-02-12 19:33:11 +0000

    pyramids

---
+++ Main.java
@@ -0,0 +1,88 @@
+public class Main {
+    public static void main(String[] args) {
+
+          // int num = 5;
+
+           //for (int i = 1; i <= num; i++) {
+          // for (int a = 1; a <= i; a++) {
+          // System.out.print("*");
+        //}
+         //  System.out.println();
+ //}
+
+             int rows = 5, k = 0;
+             for (int i = 1; i <= rows; ++i, k = 0) {
+             for (int space = 1; space <= rows - i; ++space) {
+             System.out.print("  ");
+            }
+
+             while (k != 2 * i - 1) {
+             System.out.print("* ");
+               ++k;
+             }
+             System.out.println();
+          }
+
+
+
+        //  int rows = 5;
+
+        //  for (int i = rows; i >= 1; --i) {
+        //  for (int space = 1; space <= rows - i; ++space) {
+        //  System.out.print("  ");
+        //}
+
+        //   for (int j = i; j <= 2 * i - 1; ++j) {
+        //   System.out.print("* ");
+        // }
+
+        //   for (int j = 0; j < i - 1; ++j) {
+        //   System.out.print("* ");
+        //}
+
+        //    System.out.println();
+        //}
+
+
+
+        //    int size = 5;
+        //    int num = 1;
+        //    for (int i = 0; i < size; i++) {
+        //    for (int j = 0; j < size-i-1; j++) {
+        //    System.out.print(" ");
+        //     }
+        //    for (int k = 0; k < 2*i+1; k++) {
+        //    System.out.print(num++);
+        //    }
+        //    num = 1;
+        //    System.out.println();
+        //  }
+
+
+
+       //     int size = 5;
+        //    for (int i = 1; i <= size; i++) {
+          //  for (int j = size; j > i; j--) {
+        //    System.out.print(" ");
+            //}
+            //for (int k = 0; k < i * 2 - 1; k++) {
+        //    System.out.print("*");
+            //}
+            //System.out.println();
+        //}
+        //    for (int i = 1; i <= size - 1; i++) {
+            //for (int j = 0; j < i; j++) {
+        //    System.out.print(" ");
+            //}
+            //for (int k = (size - i) * 2 - 1; k > 0; k--) {
+        //    System.out.print("*");
+            //}
+            //System.out.println();
+        //}
+
+
+
+    }
+}
+
+
